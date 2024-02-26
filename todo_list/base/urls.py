8b5c806b285ad53a6ba794e7fa27b255bb036b39
VyacheslavDotsenko <dotsenko.01@list.ru>
2024-02-26 13:25:03 +0300

    Create Class views

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.tasklist, name='tasks')
+    path('', views.TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
+    path('create-task/', views.TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', views.TaskUpdate.as_view(), name='task-update'),
 ]
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView
+from django.views.generic.list import ListView
+from base.models import Task
+
+
 # Create your views here.
-def tasklist(request):
-    return HttpResponse("To-do list")
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
